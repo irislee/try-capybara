c73cd6773e4416dd52aba4abd974c7320040865b
irislee <irislee513@gmail.com>
2013-11-01 23:14:56 -0400

    edit readme

--- spec/features_spec.rb
+++ spec/features_spec.rb
@@ -1,15 +1,15 @@
 require_relative '../simple_app.rb'
 require 'spec_helper.rb'
 
-# describe 'google search page', :js => true do 
-#   it "should search the google for something" do
-#     visit 'http://google.com'
-#     fill_in("gbqfq", :with => "Flatiron School")
-#     sleep 2
-#     click_button("gbqfb")
-#     sleep 2
-#   end
-# end
+describe 'google search page', :js => true do 
+  it "should search the google for something" do
+    visit 'http://google.com'
+    fill_in("gbqfq", :with => "Flatiron School")
+    sleep 2
+    click_button("gbqfb")
+    sleep 2
+  end
+end
 
 describe 'youtube search page', :js => true do 
   it "should search youtube for maru" do
